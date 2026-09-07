f13f940fcdd3344068012bdd5412f5cd2b10bd87
muellerthomaswd-prog <muellerthomaswd@gmail.com>
2026-09-07 16:31:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lernquiz-shell-v1";
+const CACHE = "lernquiz-shell-v2";
 const SHELL = ["./", "index.html", "style.css", "app.js", "manifest.json", "icon.svg"];
 
 self.addEventListener("install", (e) => {
